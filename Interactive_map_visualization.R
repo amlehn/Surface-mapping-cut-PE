868a6f142be93d9aff8ca5a9866c43d1a4fbb466
Andrea M Lehn <andreamlehn@dhcp-10-29-228-215.dyn.MIT.EDU>
2023-10-02 12:10:33 -0400

    test commit

--- Interactive_map_visualization.R
+++ Interactive_map_visualization.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(here)
 library(viridis)
 library(plotly)
+
 #use map scan to generate heat map
 mapDir <- here("..", "Surface mapping cut PE","Processed", "MapData")
 mapFile <- file.path(mapDir,"UV00_050223_500_S1_map_Processed.csv")
@@ -15,22 +16,25 @@ mapData <- mapData %>%
          y = Y.mm.,
          depth = depth.um.) 
 
-mapData <- mapData%>%
-  select(c(x,y,depth)) %>%
-  filter(y <= 3)
+#mapData <- mapData %>%
+#  select(c(x,y,depth)) %>%
+#  filter(y <= 3)
 
 xRange <- range(mapData$x, na.rm = TRUE)
 yRange <- range(mapData$y, na.rm = TRUE)
 aspectRatio <- diff(yRange) / diff(xRange)
 
-p <- ggplot(mapData, aes(x = x, y = y, fill = depth)) +
-  geom_raster( interpolate = TRUE) + 
-  scale_fill_viridis(option = "deep", direction = -1) +
-  geom_contour(aes(z = depth), binwidth = 10, color = "white") +
+p <- ggplot(mapData, aes(x = x, y = y, fill = depth))+
+  #geom_rect(aes(xmin = min(x), xmax = max(x),
+  #             ymin = min(y), ymax = max(y)))+ 
+  geom_tile()+
+  scale_fill_viridis(option = "turbo", direction = -1)+
+  geom_contour(aes(z = depth), binwidth = 10, color = "white",
+               linewidth = 0.1)+
   coord_fixed(ratio = aspectRatio) +
   theme_minimal() + 
-  theme(axis.text = element_text(size = 12), 
-        axis.title = element_text(size = 14)) +
+  theme(axis.text = element_text(size = 10), 
+        axis.title = element_text(size = 10)) +
   labs(x = "X", 
        y = "Y", 
        fill = "Depth",
